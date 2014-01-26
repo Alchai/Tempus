7ab89de36f64ba9a2d616a2cd595a92adf89ed52
Retro11231 <dstoves11231@yahoo.com>
2014-01-25 23:46:27 -0500

    Initial Audio Test - menu music
    Paul

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,14 +6,24 @@ public class AudioManager : MonoBehaviour
     //private AudioClip[] sounds;
     //private AudioClip[] music;
 
-    System.Collections.Generic.Dictionary<string, AudioClip> sounds;
-    System.Collections.Generic.Dictionary<string, AudioClip> music;
+   static System.Collections.Generic.Dictionary<string, AudioClip> sounds;
+   static System.Collections.Generic.Dictionary<string, AudioClip> music;
+   static AudioSource p1Source, p2Source, p3Source, p4Source;
+   static Player playerRef;
+   static Client clientRef;
 
-    AudioSource p1Source, p2Source, mainSource;
+    [SerializeField]
+   private bool mainMenus = false;
 
 	// Use this for initialization
 	void Start () 
     {
+        AudioSource[] audioSources =  this.GetComponents<AudioSource>();
+        p1Source = audioSources[0];
+        p2Source = audioSources[1];
+        p3Source = audioSources[2];
+        p4Source = audioSources[3];
+
         sounds = new System.Collections.Generic.Dictionary<string, AudioClip>();
         music = new System.Collections.Generic.Dictionary<string, AudioClip>();
 
@@ -29,30 +39,178 @@ public class AudioManager : MonoBehaviour
             music.Add(tempMusArray[curMusIndex].name, tempMusArray[curMusIndex]);
         }
 
-        //playBGM();
+        playBGM();
+
+        //Get a ref to the player
+        if (!mainMenus)
+        {
+            playerRef = GameObject.Find("me").GetComponent<Player>();
+            if(playerRef)
+                clientRef = playerRef.client;
+
+            MuteBGM();
+            //Debug.Log("MUTE");
+
+            if (clientRef)
+            {
+                if (clientRef.myChar == 0 || clientRef.hisChar == 0)
+                {
+                    p1Source.mute = false;
+                    p1Source.volume = 0.5f;
+                }
+                if (clientRef.myChar == 1 || clientRef.hisChar == 1)
+                {
+                    p2Source.mute = false;
+                    p2Source.volume = 0.5f;
+                }
+                if (clientRef.myChar == 2 || clientRef.hisChar == 2)
+                {
+                    p3Source.mute = false;
+                    p3Source.volume = 0.5f;
+                }
+                if (clientRef.myChar == 3 || clientRef.hisChar == 3)
+                {
+                    p4Source.mute = false;
+                    p4Source.volume = 0.5f;
+                }
+            }
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
+        if (!mainMenus)
+        {
+            if (playerRef)
+                updateBGM(playerRef.GetWinBackground());
+            else
+                playerRef = GameObject.Find("me").GetComponent<Player>();
+        }
 	}
 
-    public void play(string soundName, float volume, Vector3 position)
+   static public void play(string soundName, float volume, Vector3 position)
     {
         AudioSource.PlayClipAtPoint(sounds[soundName], position, volume);
     }
+
+   static public void playAll()
+   {
+       if (p1Source)
+       {
+           p1Source.Play();
+       }
+       if (p2Source)
+       {
+           p2Source.Play();
+           p2Source.time = p1Source.time;
+       }
+       if (p3Source)
+       {
+           p3Source.Play();
+           p3Source.time = p2Source.time;
+       }
+       if (p4Source)
+       {
+           p4Source.Play();
+           p4Source.time = p3Source.time;
+       }
+   }
 
     //TODO: put two audiosources on camera, one for each player
     //      Begin playing all sounds on start, set their volume based upon current hp/tugowar level
     //      
     private void playBGM()
     {
-
+        if (p1Source)
+        {
+            p1Source.Play();
+        }
+        if (p2Source)
+        {
+            p2Source.Play();
+            p2Source.time = p1Source.time;
+        }
+        if (p3Source)
+        {
+            p3Source.Play();
+            p3Source.time = p2Source.time;
+        }
+        if (p4Source)
+        {
+            p4Source.Play();
+            p4Source.time = p3Source.time;
+        }
     }
 
-    private void updateBGM(float player1Weight, float player2Weight)
+    private void MuteBGM()
     {
-        p1Source.volume = player1Weight;
-        p2Source.volume = player2Weight;
+        p4Source.mute = p3Source.mute = p2Source.mute = p1Source.mute = true;
     }
+
+    #region Update
+    private void updateBGM(float player1Weight)
+    {
+        if (clientRef.myChar == 0 && clientRef.hisChar == 1)
+        {
+            p1Source.volume = player1Weight;
+            p2Source.volume = playerRef.GetMaxLevelState() - player1Weight;
+        }
+        else if (clientRef.myChar == 0 && clientRef.hisChar == 2)
+        {
+            p1Source.volume = player1Weight;
+            p3Source.volume = playerRef.GetMaxLevelState() - player1Weight;
+        }
+        else if (clientRef.myChar == 0 && clientRef.hisChar == 3)
+        {
+            p1Source.volume = player1Weight;
+            p4Source.volume = playerRef.GetMaxLevelState() - player1Weight;
+        }
+        else if (clientRef.myChar == 1 && clientRef.hisChar == 0)
+        {
+            p2Source.volume = player1Weight;
+            p1Source.volume = playerRef.GetMaxLevelState() - player1Weight;
+        }
+        else if (clientRef.myChar == 1 && clientRef.hisChar == 2)
+        {
+            p2Source.volume = player1Weight;
+            p3Source.volume = playerRef.GetMaxLevelState() - player1Weight;
+        }
+        else if (clientRef.myChar == 1 && clientRef.hisChar == 3)
+        {
+            p2Source.volume = player1Weight;
+            p4Source.volume = playerRef.GetMaxLevelState() - player1Weight;
+        }
+        else if (clientRef.myChar == 2 && clientRef.hisChar == 0)
+        {
+            p3Source.volume = player1Weight;
+            p1Source.volume = playerRef.GetMaxLevelState() - player1Weight;
+        }
+        else if (clientRef.myChar == 2 && clientRef.hisChar == 1)
+        {
+            p3Source.volume = player1Weight;
+            p2Source.volume = playerRef.GetMaxLevelState() - player1Weight;
+        }
+        else if (clientRef.myChar == 2 && clientRef.hisChar == 3)
+        {
+            p3Source.volume = player1Weight;
+            p4Source.volume = playerRef.GetMaxLevelState() - player1Weight;
+        }
+        else if (clientRef.myChar == 3 && clientRef.hisChar == 0)
+        {
+            p4Source.volume = player1Weight;
+            p1Source.volume = playerRef.GetMaxLevelState() - player1Weight;
+        }
+        else if (clientRef.myChar == 3 && clientRef.hisChar == 0)
+        {
+            p4Source.volume = player1Weight;
+            p2Source.volume = playerRef.GetMaxLevelState() - player1Weight;
+        }
+        else if (clientRef.myChar == 3 && clientRef.hisChar == 0)
+        {
+            p4Source.volume = player1Weight;
+            p3Source.volume = playerRef.GetMaxLevelState() - player1Weight;
+        }
+    }
+    #endregion
 }
